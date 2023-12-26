e9b78f805a9ef202d9c26072e34b83c5b6d68d52
진미나 <mina200238@gmail.com>
2023-12-26 14:57:23 +0900

    Feat: add bookMark getMyApplication

---
+++ src/routers/bookMarkRouter.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+const {
+  getMyMatchingPostInfo,
+  getMyApplicationList,
+} = require('../controllers/bookMarkController');
+
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
+const router = Router();
+
+//북마크 관련 기능
+// 매칭중인 매칭신청글 별 요청자수 목록
+// router.get(
+//   '/myMatchingPostInfo',
+//   jwtMiddleware.authenticateToken,
+//   getMyMatchingPostInfo,
+// );
+
+// 내가 지원한 매칭포스트의 정보
+router.get(
+  '/myApplicationList',
+  jwtMiddleware.authenticateToken,
+  getMyApplicationList,
+);
+
+module.exports = router;
