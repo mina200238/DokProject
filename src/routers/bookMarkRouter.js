22247506998da7eed8700dbacfd21d5c6e6e2c4d
진미나 <109504714+pado0711@users.noreply.github.com>
2024-01-11 15:08:18 +0900

    Revert "Feature/matching"

--- src/routers/bookMarkRouter.js
+++ src/routers/bookMarkRouter.js
@@ -7,13 +7,13 @@ const {
 const jwtMiddleware = require('../middlewares/jwtMiddleware');
 const router = Router();
 
-// 북마크 관련 기능
+//북마크 관련 기능
 // 매칭중인 매칭신청글 별 요청자수 목록
-router.get(
-  '/myMatchingPostInfo',
-  jwtMiddleware.authenticateToken,
-  getMyMatchingPostInfo,
-);
+// router.get(
+//   '/myMatchingPostInfo',
+//   jwtMiddleware.authenticateToken,
+//   getMyMatchingPostInfo,
+// );
 
 // 내가 지원한 매칭포스트의 정보
 router.get(
