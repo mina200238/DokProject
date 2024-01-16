7de00869429b173254f60fff6a27109d65dfdf9b
진미나 <mina200238@gmail.com>
2024-01-16 23:56:05 +0900

    fix: modified UserUpdatePasswordRequest class

--- src/dtos/users/userUpdatePasswordRequest.js
+++ src/dtos/users/userUpdatePasswordRequest.js
@@ -4,23 +4,45 @@ const { BadRequestError } = require('../../errors/badReqestError');
 
 class UserUpdatePasswordRequest {
   constructor(body) {
-    this.verify(body.password, body.confirmPassword);
-    this.password = body.password;
-    this.confirmPassword = body.confirmPassword;
+    this.verify(
+      body.currentPassword,
+      body.newPassword,
+      body.confirmNewPassword,
+    );
+    //현재 비밀번호
+    this.currentPassword = body.currentPassword;
+    //변경할 새 비밀번호
+    this.newPassword = body.newPassword;
+    //새 비밀번호 확인
+    this.confirmNewPassword = body.confirmNewPassword;
   }
 
-  verify(password, confirmPassword) {
-    if (password !== confirmPassword) {
-      throw new BadRequestError('비밀번호가 일치하지 않습니다.');
+  verify(currentPassword, newPassword, confirmNewPassword) {
+    if (!currentPassword) {
+      throw new BadRequestError('현재 비밀번호를 작성해주세요.');
+    }
+    if (!newPassword) {
+      throw new BadRequestError('변경할 비밀번호를 작성해주세요.');
+    }
+    if (!confirmNewPassword) {
+      throw new BadRequestError('변경할 비밀번호를 작성해주세요.');
+    }
+
+    if (newPassword !== confirmNewPassword) {
+      throw new BadRequestError('새 비밀번호 확인란을 정확히 작성해주세요.');
     }
   }
 
-  getPassword() {
-    return this.password;
+  getCurrentPassword() {
+    return this.currentPassword;
   }
 
-  getConfirmPassword() {
-    return this.confirmPassword;
+  getNewPassword() {
+    return this.newPassword;
+  }
+
+  getconfirmNewPassword() {
+    return this.confirmNewPassword;
   }
 }
 
